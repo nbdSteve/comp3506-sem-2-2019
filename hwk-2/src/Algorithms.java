125c3da6c596562a93debe19d1b32cf22e5be582
stevegoodhill <s.goodhill@uq.net.au>
2019-08-22 14:00:28 +1000

    Finished algorithms, just need to write up README

--- hwk-2/src/Algorithms.java
+++ hwk-2/src/Algorithms.java
@@ -6,12 +6,46 @@ import java.util.Queue;
 public class Algorithms {
 
     /**
-     * Write your implementation of the sortQueue algorithm here
+     * Write your implementation of the sortQueue algorithm her
      *
      * @param queue the queue to sort
      */
     public static <T extends Comparable<T>> void sortQueue(Queue<T> queue) {
-
+        int iterator = 1;
+        T first;
+        T next;
+        do {
+            first = queue.remove();
+            for (int i = 0; i < queue.size(); i++) { //O(n)
+                next = queue.remove();
+                if (first.compareTo(next) > 0) {
+                    queue.add(next);
+                } else {
+                    queue.add(first);
+                    first = next;
+                }
+            }
+            queue.add(first);
+            iterator++;
+        } while (iterator < queue.size()); //O(n-1)
+//        System.out.println(queue.toString());
+//        boolean change = true;
+//        String firstString;
+//        do {
+//            T first = queue.remove();
+//            firstString = first.toString();
+//            for (int i = 0; i < queue.size(); i++) { //O(n)
+//                T next = queue.remove();
+//                if (first.compareTo(next) > 0) {
+//                    queue.add(next);
+//                } else {
+//                    queue.add(first);
+//                    first = next;
+//                }
+//                if (firstString.equals(first.toString())) change = false;
+//            }
+//            queue.add(first);
+//        } while (change); //O(n-1)
     }
 
     /**
@@ -21,6 +55,12 @@ public class Algorithms {
      * @return the missing number in the sequence
      */
     public static int findMissingNumber(int[] numbers) {
-        return 0;
+        int difference = numbers[1] - numbers[0];
+        return recursive(numbers, 0, difference);
+    }
+
+    public static int recursive(int[] numbers, int index, int difference) {
+        if (numbers[index] + difference != numbers[index + 1]) return numbers[index] + difference;
+        return recursive(numbers, index + 1, difference);
     }
 }
